c4e450ceb1f191d90c06b1d1c6b9efd756a391fd
Eric Smith <ericmsmith@gmail.com>
2015-06-08 17:42:36 -0400

    Joining all Regents exam data 2007 onward

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # Variables
 #year_l = range(2004, 2015)
-year_l = range(2013, 2015)
+year_l = range(2007, 2015)
 regents_table_s_d = {2004: 'Regents',
                      2005: 'Regents',
                      2006: 'Regents Results 2005-06',
--- join_data.py
+++ join_data.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env
 """
 Join all data in SQL databases
 """
@@ -10,7 +11,15 @@ reload(utilities)
 
 
 def main():
+    
+    con = utilities.connect_to_sql('temp')    
+    with con:
+        cur = con.cursor()
 
+        find_regents_school_key(cur)
+        for year in config.year_l:
+            find_regents_pass_rate(cur, year, config.regents_table_s_d[year])
+    
     con = utilities.connect_to_sql('joined')
     with con:
         cur = con.cursor()
@@ -19,43 +28,83 @@ def main():
 
 
 
+def find_regents_school_key(cur):
+    """ Creates a table of each school ID and name """
+    
+    command_s = 'DROP TABLE IF EXISTS school_key;'
+    cur.execute(command_s)
+    command_s = """CREATE TABLE school_key
+SELECT ENTITY_CD, ENTITY_NAME FROM SRC{0:d}.`{1}`
+WHERE YEAR = {0:d} AND SUBJECT = 'REG_ENG' AND SUBGROUP_NAME = 'General Education'"""
+    # The REG_ENG is kind of a hack
+    command_s = command_s.format(config.year_l[-1],
+                                 config.regents_table_s_d[config.year_l[-1]])
+    cur.execute(command_s)
+    command_s = """ALTER TABLE school_key
+ADD INDEX ENTITY_CD (ENTITY_CD)"""
+    cur.execute(command_s)
+    
+
+
 def find_regents_pass_rate(cur, year, table_s):
     """ Returns an N-by-3 of the ENTITY_CD, SUBJECT, and pass rate """
     
-    command_s = 'DROP TABLE IF EXISTS temp.SRC{0:d};'
+    print('Starting find_regents_pass_rate for year {:d}'.format(year))
+    
+    command_s = 'DROP TABLE IF EXISTS temp{0:d};'
     cur.execute(command_s.format(year))
-    command_s = 'INSERT INTO temp.SRC{0:d} SELECT * FROM SRC{0:d}.{1};'
+    command_s = 'CREATE TABLE temp{0:d} SELECT * FROM SRC{0:d}.`{1}`;'
     cur.execute(command_s.format(year, table_s))
-    command_s = """ALTER TABLE temp.SRC{0:d}
-CHANGE ENTITY_CD ENTITY_CD_{0:d}
-CHANGE SUBJECT SUBJECT_{0:d}
-CHANGE `PER_65-84` `PER_65-84` INT
-CHANGE `PER_85-100` `PER_85-100` INT
-ADD percent_passing_{0:d};"""
+    command_s = "DELETE FROM temp{0:d} WHERE `PER_65-84` = 's' OR `PER_85-100` = 's';"
     cur.execute(command_s.format(year))
-    command_s = """UPDATE temp.SRC{0:d}
+    command_s = """ALTER TABLE temp{0:d} CHANGE ENTITY_CD ENTITY_CD_{0:d} CHAR(12);"""
+    cur.execute(command_s.format(year))
+    command_s = """ALTER TABLE temp{0:d} CHANGE SUBJECT SUBJECT_{0:d} CHAR(12);"""
+    cur.execute(command_s.format(year))
+    command_s = """ALTER TABLE temp{0:d} CHANGE `PER_65-84` `PER_65-84` INT;"""
+    cur.execute(command_s.format(year))
+    command_s = """ALTER TABLE temp{0:d} CHANGE `PER_85-100` `PER_85-100` INT;"""
+    cur.execute(command_s.format(year))
+    command_s = """ALTER TABLE temp{0:d} ADD percent_passing_{0:d} INT;"""
+    cur.execute(command_s.format(year))
+    command_s = """UPDATE temp{0:d}
 SET percent_passing_{0:d} = `PER_65-84` + `PER_85-100`;"""
     cur.execute(command_s.format(year))
-    command_s = """CREATE TABLE temp.SRC{0:d}_filtered
-SELECT ENTITY_CD, SUBJECT, percent_passing FROM temp
-WHERE YEAR = {0:d} AND SUBGROUP_NAME = 'All Students';"""
+    command_s = 'DROP TABLE IF EXISTS temp{0:d}_filtered;'
     cur.execute(command_s.format(year))
-    
+    print('Starting to filter for year {:d}'.format(year))
+    command_s = """CREATE TABLE temp{0:d}_filtered
+SELECT ENTITY_CD_{0:d}, SUBJECT_{0:d}, percent_passing_{0:d} FROM temp{0:d}
+WHERE YEAR = {0:d} AND SUBGROUP_NAME = 'General Education';"""
+    cur.execute(command_s.format(year))
+    command_s = 'DROP TABLE IF EXISTS temp{0:d}_averaged;'
+    cur.execute(command_s.format(year))
+    command_s = """CREATE TABLE temp{0:d}_averaged
+SELECT ENTITY_CD_{0:d}, AVG(percent_passing_{0:d}) FROM temp{0:d}_filtered
+WHERE SUBJECT_{0:d} IN ('REG_GLHIST', 'REG_USHG_RV', 'REG_ENG', 'REG_INTALG', 'REG_ESCI_PS', 'REG_LENV')
+GROUP BY ENTITY_CD_{0:d};"""
+    cur.execute(command_s.format(year))
+    command_s = """ALTER TABLE temp{0:d}_averaged
+ADD INDEX ENTITY_CD_{0:d} (ENTITY_CD_{0:d})"""
+    cur.execute(command_s.format(year))
         
 
 
 def join_regents_pass_rate(cur):
     """ Join the percentage of students passing the Regents exam """
     
+    print('Starting join_regents_pass_rate')
+    
     cur.execute('DROP TABLE IF EXISTS regents_pass_rate')
-    for year in config.year_l:
-        find_regents_pass_rate(cur, year, config.regents_table_s_d[year])
     command_s = """CREATE TABLE regents_pass_rate
-SELECT * FROM temp.SRC{0:d}_filtered""".format(config.year_l[0])
-    for year in config.year_l[1:]:
+SELECT * FROM temp.school_key"""
+    for year in config.year_l:
         this_table_command_s = """
-INNER JOIN temp.SRC{1:d}_filtered
-ON temp.SRC{0:d}_filtered.ENTITY_CD_{0:d} = temp.SRC{1:d}_filtered.ENTITY_CD_{1:d}""".format(config.year_l[0], year)
+INNER JOIN temp.temp{2:d}_averaged
+ON temp.school_key.ENTITY_CD = temp.temp{2:d}_averaged.ENTITY_CD_{2:d}"""
+        this_table_command_s = this_table_command_s.format(config.year_l[-1],
+            config.regents_table_s_d[config.year_l[-1]],
+            year)
         command_s += this_table_command_s
     command_s += ';'
     cur.execute(command_s)
