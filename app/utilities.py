01f158bd21d222341150122d076de36b2991d2d6
Eric Smith <ericmsmith@gmail.com>
2015-06-21 12:14:51 -0400

    Config changes

--- app/utilities.py
+++ app/utilities.py
@@ -6,12 +6,14 @@ import numpy as np
 import pymysql as mdb
 from sqlalchemy import create_engine
 
+import config_unsynced
+
     
     
 def connect_to_sql(database_s):
     """ Connect to MySQL server. Returns connection object and cursor. """
 
-    con = mdb.connect('localhost', 'root', '', database_s) #host, user, password, #database
+    con = mdb.connect('localhost', 'root', config_unsynced.pword_s, database_s) #host, user, password, #database
     
     return con
     
@@ -48,7 +50,10 @@ def select_data(con, cur, field_s_l, table_s, output_type='np_array'):
 def write_to_sql_table(df, table_s, database_s):
     """ Writes DataFrame df as a new table table_s in SQL. Assumes no existing connection to a MySQL database!"""
     
-    engine = create_engine('mysql+pymysql://root:@localhost/' + database_s, echo=False)
+    engine = create_engine('mysql+pymysql://root:{0}@localhost/'\
+                           .format(config_unsynced.pword_s) + \
+                           database_s,
+                           echo=False)
     engine.execute('DROP TABLE IF EXISTS {0};'.format(table_s))
     cnx = engine.raw_connection()
     df.to_sql(name=table_s, con=cnx, flavor='mysql', if_exists='replace')
